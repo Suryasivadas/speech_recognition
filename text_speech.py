94fabbc3c9cdb1889ae74f22a2008e12dc0321d8
SURYA SIVADAS <suryaagesh67@gmail.com>
2024-04-06 12:38:03 +0530

    initial commit

---
+++ text_speech.py
@@ -0,0 +1,11 @@
+# pip install pyttsx3
+
+import pyttsx3
+txt_sp=pyttsx3.init()
+text=input('enter the text......\n')
+# voices=txt_sp.getProperty('voices')
+# for voice in voices:
+#     print('ID:',voice.id)
+         
+# txt_sp.say(text)
+# txt_sp.runAndWait()
